ea44260df9f4428daac37a4c33016c8d95c5cd9f
Mark Mauro <Mark_Mauro@anfcorp.com>
2024-12-02 10:59:09 -0500

    Initial Commit

---
+++ PostureMaxApp/PostureMaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureMaxAppApp.swift
+//  PostureMaxApp
+//
+//  Created by Mark Mauro on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PostureMaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostureMaxAppTests/PostureMaxAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostureMaxAppTests.swift
+//  PostureMaxAppTests
+//
+//  Created by Mark Mauro on 12/2/24.
+//
+
+import Testing
+@testable import PostureMaxApp
+
+struct PostureMaxAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
